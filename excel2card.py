b40a06fa6f8c6fe0c4bdfc7b0d16ef1b6086e5d6
宍戸成太 <johta@shishidonaritanoMacBook-Pro.local>
2020-03-17 11:13:05 +0900

    曜日に応じて色変更

--- excel2card.py
+++ excel2card.py
@@ -6,32 +6,55 @@ import os
 
 font = ImageFont.truetype('/Library/Fonts/ヒラギノ角ゴシック W0.ttc', 30)
 font_small = ImageFont.truetype('/Library/Fonts/ヒラギノ角ゴシック W0.ttc', 20)
+
+
+# 曜日の値を参照して色を生成する関数
+def setColor(day):
+    if day == None:
+        return (255, 255, 255)
+    elif day == "月":
+        return (255, 248, 181)
+    elif day == "火":
+        return (235, 191, 175)
+    elif day == "水":
+        return (136, 190, 210)
+    elif day == "木":
+        return (186, 217, 158)
+    else :
+        return (144, 144, 144)
+
+# エクセルを読み込んでカード画像を生成する関数
 def drawCard():
     #エクセル読み込み（ファイル名・シート名はハードコード）
     workbook = openpyxl.load_workbook('cardsheet.xlsx')
     sheet = workbook["Sheet1"]
 
-    for i in range(2,4):
+    for i in range(2,30):
         card_num = "card_"+str(i)
-        print(card_num)
+        # print(card_num)
 
         id = sheet.cell(row=i, column=1).value
         title = sheet.cell(row=i, column=2).value
         cost =sheet.cell(row=i, column=5).value
         description = sheet.cell(row=i, column=3).value
         flavor =sheet.cell(row=i, column=4).value
-        print(flavor)
+        dayOfWeek = flavor =sheet.cell(row=i, column=9).value
 
-        im = Image.new("RGB",(708,1033),(255,255,255))
+        color = setColor(dayOfWeek)
+
+        print(dayOfWeek+","+str(color))
+
+        im = Image.new("RGB",(708,1033),color)
         draw = ImageDraw.Draw(im)
 
-        draw.rectangle((2, 2, 706, 1030), fill=(255, 255, 255) ,outline=(0,0,0)) #Frame
+        # draw.rectangle((2, 2, 706, 1030) ,outline=(0,0,0)) #Frame
         draw.rectangle((10, 10, 100, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Number
         draw.rectangle((110, 10, 610, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Title
         draw.rectangle((620, 10, 698, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Cost
         draw.rectangle((40, 110, 668, 500), fill=(255, 255, 255) ,outline=(0,0,0)) #illust
         draw.rectangle((40, 520, 668, 900), fill=(255, 255, 255) ,outline=(0,0,0))#description
         draw.rectangle((40, 920, 668, 1000), fill=(255, 255, 255) ,outline=(0,0,0))#flavor text
+
 
         draw.multiline_text((30, 30), id, fill=(0, 0, 0), font=font)
 
@@ -67,7 +90,7 @@ def drawCard():
 
         #でかすぎるのでリサイズ
 #        im_resize = im.resize((int(im.width*0.48), int(im.height*0.48)),Image.LANCZOS)
-#        im_resize.save('card/'+card_num+'.jpg', quality=95)
+#        im_resize.save('card/'+id+'.jpg', quality=95)
 
 if __name__ == '__main__':
     drawCard()
