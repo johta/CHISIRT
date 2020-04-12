ebb2f928fe8622e099543df4ca4bd6485cfa9a3b
johta <johta.sisido@gmail.com>
2020-04-12 15:23:03 +0900

    色々直した

--- Excel2Cards/excel2card.py
+++ Excel2Cards/excel2card.py
@@ -14,7 +14,9 @@ font_big = ImageFont.truetype('/Library/Fonts/ヒラギノ角ゴシック 
 font_small = ImageFont.truetype('/Library/Fonts/ヒラギノ角ゴシック W4.ttc', 20)
 
 
-# 曜日の値を参照して色を生成する関数
+'''
+　曜日の値を参照して色を生成する関数
+'''
 def setColor(day):
     if day == None: #何も入ってなければまっしろ *入力漏れ発見用
         return (255, 255, 255)
@@ -29,7 +31,10 @@ def setColor(day):
     else : #わけのわからない値が入ってたらグレー *入力ミス発見用
         return (144, 144, 144)
 
-# エクセルを読み込んで表面のカード画像を生成する関数
+
+'''
+　エクセルを読み込んで表面のカード画像を生成する関数
+'''
 def generateFrontCard():
     print("generating card(front)...")
 
@@ -37,34 +42,22 @@ def generateFrontCard():
     workbook = openpyxl.load_workbook('cardsheet.xlsx')
     sheet = workbook["sheet1"]
 
-    # print("workbook:"+workbook.name)
-
     for i in range(2,num):
         id = sheet.cell(row=i, column=1).value
         title = sheet.cell(row=i, column=2).value
         cost = sheet.cell(row=i, column=4).value
         description = sheet.cell(row=i, column=3).value
-        # flavor = sheet.cell(row=i, column=4).value
         dayOfWeek =sheet.cell(row=i, column=8).value
-
         color = setColor(dayOfWeek)
-
-        #デバッグ用
-        # print(id+","+title+","+description+","+dayOfWeek+","+str(color))
 
         im = Image.new("RGB",(708,1033),color)
         draw = ImageDraw.Draw(im)
-
-        # draw.rectangle((2, 2, 706, 1030) ,outline=(0,0,0)) #Frame
         draw.rectangle((10, 10, 100, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Number
         draw.rectangle((110, 10, 610, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Title
         draw.rectangle((620, 10, 698, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Cost
         draw.rectangle((40, 110, 668, 500), fill=(255, 255, 255) ,outline=(0,0,0)) #illust
         draw.multiline_text((280, 260), "illust", fill=(0, 0, 0), font=font_big)
         draw.rectangle((40, 520, 668, 850), fill=(255, 255, 255) ,outline=(0,0,0))#description
-        # draw.rectangle((40, 870, 668, 1015), fill=(255, 255, 255) ,outline=(0,0,0))#flavor text
-
-        # im.show()
 
         #カードIDの印字
         draw.multiline_text((25, 30), id, fill=(0, 0, 0), font=font_big)
@@ -85,29 +78,24 @@ def generateFrontCard():
             draw.multiline_text((50, y+450),line, fill=(0,0,0), font=font)  # 1行分の文字列を画像に描画
             line_counter = line_counter +1  # 行数のカウンターに1
 
+        # 工数の印字
         draw.multiline_text((645, 30), str(cost), fill=(0, 0, 0), font=font_big)
-
-        #フレーバーテキストの印字（折返し有り）
-        # wrap_list = textwrap.wrap(flavor, 30)
-        # line_counter = 0  # 行数のカウンター
-        # for line in wrap_list:  # wrap_listから1行づつ取り出しlineに代入
-        #     y = line_counter*35+80  # y座標をline_counterに応じて下げる
-        #     draw.multiline_text((50, y+800),line, fill=(0,0,0), font=font_small)  # 1行分の文字列を画像に描画
-        #     line_counter = line_counter +1  # 行数のカウンターに1
 
         if flag_view==True:
             # 確認用
             im.show()
-            # print("flag_view is "+str(flag_view))
-
         else :
             #でかすぎるのでリサイズ　*LANCZOSなら劣化少なく縮小できるらしい
             im_resize = im.resize((int(im.width*0.48), int(im.height*0.48)),Image.LANCZOS)
             im_resize.save('front/'+id+'.jpg', quality=95)
-            # print("flag_view is "+str(flag_view))
+
     print("done!")
 
+
+'''
 #エクセルを読み込んで裏面を生成する関数
+
+'''
 def generateRearCard():
     print("generating card(rear)...")
 
@@ -122,12 +110,10 @@ def generateRearCard():
         result = sheet.cell(row=i, column=6).value  #Result
         flavor = sheet.cell(row=i, column=7).value  #Flavor
         dayOfWeek =sheet.cell(row=i, column=8).value#DayOfWeek
-
         color = setColor(dayOfWeek)
 
         im = Image.new("RGB",(708,1033),color)
         draw = ImageDraw.Draw(im)
-
         draw.rectangle((10, 10, 100, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Number
         draw.rectangle((110, 10, 610, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Title
         draw.rectangle((620, 10, 698, 90), fill=(255, 255, 255) ,outline=(0,0,0)) #Point
@@ -136,7 +122,7 @@ def generateRearCard():
         draw.rectangle((40, 520, 668, 800), fill=(255, 255, 255) ,outline=(0,0,0))#Result
         draw.rectangle((40, 820, 668, 1015), fill=(255, 255, 255) ,outline=(0,0,0))#flavor text
 
-
+        # カードIDの印字
         draw.multiline_text((25, 30), id, fill=(0, 0, 0), font=font_big)
 
         #カード名の印字（折返し有り）
@@ -155,6 +141,7 @@ def generateRearCard():
             draw.multiline_text((50, y+450),line, fill=(0,0,0), font=font)  # 1行分の文字列を画像に描画
             line_counter = line_counter +1  # 行数のカウンターに1
 
+        # ポイントの印字
         draw.multiline_text((625, 30), str(point), fill=(0, 0, 0), font=font_big)
 
         #フレーバーテキストの印字（折返し有り）
@@ -167,14 +154,11 @@ def generateRearCard():
 
         if flag_view==True:
             # 確認用
-            # print("flag_view is "+str(flag_view))
             im.show()
         else :
             #でかすぎるのでリサイズ　*LANCZOSなら劣化少なく縮小できるらしい
             im_resize = im.resize((int(im.width*0.48), int(im.height*0.48)),Image.LANCZOS)
             im_resize.save('front/'+id+'.jpg', quality=95)
-            # 確認用
-            # print("flag_view is "+str(flag_view))
 
     print("done!")
 
